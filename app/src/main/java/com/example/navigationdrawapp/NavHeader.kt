0f53ba96bfa627af49ff98b64cd6d35728be1844
domin <dominika.github@gmail.com>
2022-04-27 23:27:25 +0200

    added project

---
+++ app/src/main/java/com/example/navigationdrawapp/NavHeader.kt
@@ -0,0 +1,11 @@
+package com.example.navigationdrawapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavHeader : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nav_header)
+    }
+}
---
+++ app/src/main/java/com/example/navigationdrawapp/NavMenu.kt
@@ -0,0 +1,11 @@
+package com.example.navigationdrawapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nav_menu)
+    }
+}
